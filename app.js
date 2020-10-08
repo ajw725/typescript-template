47e0073796baaa216f13af0e548e756b78d79b5f
Andrew Weinstein <andrew@altvia.com>
2020-10-08 09:56:30 -0600

    arrays

--- app.js
+++ app.js
@@ -4,6 +4,12 @@
 // } = {
 var person = {
     name: 'Andrew',
-    age: 29
+    age: 29,
+    hobbies: ['Running', 'Music']
 };
+var favoriteFoods;
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -4,7 +4,14 @@
 // } = {
 const person = {
   name: 'Andrew',
-  age: 29
+  age: 29,
+  hobbies: ['Running', 'Music']
 };
 
+let favoriteFoods: string[];
+
 console.log(person.name);
+
+for(const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
