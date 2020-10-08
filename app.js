a6d4fef05d57d9f65325b21161b63a721f97c220
Andrew Weinstein <andrew@altvia.com>
2020-10-08 10:03:13 -0600

    tuples

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// const person: {
-//   name: string,
-//   age: number
-// } = {
 var person = {
     name: 'Andrew',
     age: 29,
-    hobbies: ['Running', 'Music']
+    hobbies: ['Running', 'Music'],
+    role: [2, 'Author']
 };
 var favoriteFoods;
 console.log(person.name);
+// person.role[1] = 10; // not allowed
+// person.role = [1, 'def', 'ghi']; // not allowed
+// person.role.push('abc'); // this one IS allowed. typescript can't catch everything
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
--- app.ts
+++ app.ts
@@ -1,16 +1,22 @@
-// const person: {
-//   name: string,
-//   age: number
-// } = {
-const person = {
+const person: {
+  name: string,
+  age: number,
+  hobbies: string[],
+  role: [number, string]
+} = {
   name: 'Andrew',
   age: 29,
-  hobbies: ['Running', 'Music']
+  hobbies: ['Running', 'Music'],
+  role: [2, 'Author']
 };
 
 let favoriteFoods: string[];
 
 console.log(person.name);
+
+// person.role[1] = 10; // not allowed
+// person.role = [1, 'def', 'ghi']; // not allowed
+// person.role.push('abc'); // this one IS allowed. typescript can't catch everything
 
 for(const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
