0cb25849463306054841c9334570e4ef29e99643
Andrew Weinstein <andrew@altvia.com>
2020-10-09 13:52:49 -0600

    class inheritance

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,22 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Department = (function () {
-    function Department(id, name, employees) {
+    function Department(id, name) {
         this.id = id;
         this.name = name;
-        this.employees = employees;
+        this.employees = [];
     }
     Department.prototype.describe = function () {
         console.log('Department: ' + this.name);
@@ -17,9 +30,38 @@ var Department = (function () {
     };
     return Department;
 }());
-var dept = new Department('1', 'Engineering', []);
+var ITDepartment = (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, 'IT') || this;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, 'Accounting') || this;
+        _this.reports = reports;
+        return _this;
+    }
+    AccountingDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var dept = new Department('d1', 'Engineering');
 console.log(dept);
 dept.hire('Jack');
 dept.hire('Jill');
 dept.printRoster();
+var itDept = new ITDepartment('d2', ['Andrew']);
+console.log(itDept);
+var accounting = new AccountingDepartment('d3', []);
+accounting.addReport('Something went wrong');
+console.log(accounting);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 class Department {
-    constructor(private readonly id: string, public name: string, private employees: string[]) {}
+    private employees: string[] = [];
+
+    constructor(private readonly id: string, public name: string) {}
 
     describe(this: Department) {
         console.log('Department: ' + this.name);
@@ -15,7 +17,27 @@ class Department {
     }
 }
 
-const dept = new Department('1', 'Engineering', []);
+class ITDepartment extends Department {
+    constructor(id: string, public admins: string[]) {
+        super(id, 'IT');
+    }
+}
+
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting');
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+    }
+
+    printReports() {
+        console.log(this.reports);
+    }
+}
+
+const dept = new Department('d1', 'Engineering');
 console.log(dept);
 
 //const deptCopy = { describe: dept.describe, name: 'foo' };
@@ -24,3 +46,10 @@ console.log(dept);
 dept.hire('Jack');
 dept.hire('Jill');
 dept.printRoster();
+
+const itDept = new ITDepartment('d2', ['Andrew']);
+console.log(itDept);
+
+const accounting = new AccountingDepartment('d3', []);
+accounting.addReport('Something went wrong');
+console.log(accounting);
