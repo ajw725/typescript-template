f86e16b09c578254a734035b3c5c8b60b5b4fa68
Andrew Weinstein <andrew@altvia.com>
2020-10-09 08:41:42 -0600

    tsconfig stuff

---
+++ dist/analytics.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log('Analyzing...');
+//# sourceMappingURL=analytics.js.map
---
+++ src/app.ts
@@ -0,0 +1,21 @@
+// let userInput: unknown;
+// let userName: string;
+//
+// userInput = 5; // ok
+// userInput = 'Andrew'; // ok
+// // userName = userInput; // NOT ok! this would work if userInput had type any
+//
+// if(typeof userInput === 'string') {
+//   userName = userInput; // ok, because we checked type first
+// }
+//
+// function generateError(message: string, errorCode: number): never {
+//   throw { message: message, errorCode: errorCode };
+// }
+//
+// generateError('An error occurred', 500);
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', () => {
+    console.log('clicked');
+});
