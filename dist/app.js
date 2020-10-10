618155094a57e11a0f3c3c925c96459730a5ac72
Andrew Weinstein <andrew@altvia.com>
2020-10-09 19:59:25 -0600

    intersection operator

--- dist/app.js
+++ dist/app.js
@@ -1,31 +1,7 @@
 "use strict";
-var add;
-add = function (n1, n2) { return n1 + n2; };
-var Person = (function () {
-    function Person(n) {
-        this.age = 29;
-        if (n) {
-            this.name = n;
-        }
-    }
-    Person.prototype.greet = function (phrase) {
-        if (this.name) {
-            console.log(phrase + ", " + this.name);
-        }
-        else {
-            console.log('hi');
-        }
-    };
-    return Person;
-}());
-var user1 = new Person('Andrew');
-user1.name = 'new name';
-user1.greet('Hello');
-var user2;
-user2 = {
-    name: 'Bodie',
-    greet: function (phrase) {
-        console.log('Hi');
-    }
+var e1 = {
+    name: 'Andrew',
+    privileges: ['create-server'],
+    startDate: new Date()
 };
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,25 @@
-// creating a function type:
-// type AddFn = (a: number, b: number) => number;
-
-// creating a function interface:
-interface AddFn {
-  (a: number, b: number): number;
-}
-
-let add: AddFn;
-add = (n1, n2) => n1 + n2;
-
-interface Named {
-  readonly name?: string;
-  outputName?: string; // optional property
-}
-
-// an interface, unlike a class, can extend *multiple* other interfaces
-interface Greetable extends Named {
-  greet(phrase: string): void;
-}
-
-class Person implements Greetable {
-  name?: string;
-  age = 29;
-
-  constructor(n?: string) {
-    if(n) {
-      this.name = n;
-    }
-  }
-
-  greet(phrase: string) {
-    if(this.name) {
-      console.log(`${phrase}, ${this.name}`);
-    } else {
-      console.log('hi');
-    }
-  }
-}
-
-// let user1: Greetable;
-// user1 = {
-//   name: 'Andrew',
-//   //age: 29,
-//   greet(phrase: string) {
-//     console.log(`${phrase}, ${this.name}`);
-//   }
-// };
-
-const user1 = new Person('Andrew');
-user1.name = 'new name';
-user1.greet('Hello');
-
-let user2: Greetable;
-user2 = {
-  name: 'Bodie',
-  greet(phrase: string) {
-    console.log('Hi');
-  }
+type Admin = {
+  name: string;
+  privileges: string[];
 };
-// user2.name = 'Bear'; // doesn't work because name is readonly
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+// use intersection operator to combine attributes of other types.
+// NOTE: also possible with interfaces, i.e. Admin and Employee interfaces, then
+// interface ElevatedEmployee extends Employee, Admin
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Andrew',
+  privileges: ['create-server'],
+  startDate: new Date()
+};
+
+// you can also do this with union types!
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
