e4d574839e9b87545bc7459ee110063dc943e1f1
Andrew Weinstein <andrew@altvia.com>
2020-10-09 20:08:51 -0600

    type guards overview

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,49 @@ var e1 = {
     privileges: ['create-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(employee) {
+    console.log("Name: " + employee.name);
+    if ('privileges' in employee) {
+        console.log("Privileges: " + employee.privileges);
+    }
+    if ('startDate' in employee) {
+        console.log("Start date: " + employee.startDate);
+    }
+}
+printEmployeeInformation(e1);
+var Car = (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log('driving...');
+    };
+    return Car;
+}());
+var Truck = (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log('driving a truck...');
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log("loading " + amount + " cargos...");
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -23,3 +23,72 @@ const e1: ElevatedEmployee = {
 type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
+
+// type guards
+function add(a: Combinable, b: Combinable) {
+  // before, we had to write a type guard using typeof like this:
+  if(typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(employee: UnknownEmployee) {
+  console.log(`Name: ${employee.name}`);
+
+  // this doesn't work because a regular Employee doesn't have privileges, BUT
+  // typeof (i.e. regular JS) doesn't know about the Employee type, so we can't use that!
+  // console.log(`Privileges: ${employee.privileges}`);
+
+  // we can do this:
+  if('privileges' in employee) {
+    console.log(`Privileges: ${employee.privileges}`);
+  }
+  if('startDate' in employee) {
+    console.log(`Start date: ${employee.startDate}`);
+  }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log('driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log(`loading ${amount} cargos...`);
+  }
+}
+
+type Vehicle = Car | Truck;
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // can't do this because only Truck has loadCargo
+  // vehicle.loadCargo();
+
+  // could do this:
+  // if('loadCargo' in vehicle) {
+  //   vehicle.loadCargo(1000);
+  // }
+
+  // but could also do this, because Car and Truck are classes:
+  if(vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
