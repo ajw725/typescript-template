d5aab8e450ecb3912dbde147775aa8a6612c391b
Andrew Weinstein <andrew@altvia.com>
2020-10-10 10:44:04 -0600

    built-in generics

--- dist/app.js
+++ dist/app.js
@@ -1,26 +1,11 @@
 "use strict";
-var _a;
-var e1 = {
-    name: 'Andrew',
-    privileges: ['create-server'],
-    startDate: new Date()
-};
-function add(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
-var result = add('abc', ' def');
-console.log(result.split(' '));
-var fetchedUserData = {
-    id: 'user1',
-    name: 'Andrew',
-};
-var title = (_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title;
-console.log(title);
-var userInput = null;
-var storedData = userInput || 'DEFAULT';
-var storedData2 = userInput !== null && userInput !== void 0 ? userInput : 'DEFAULT';
-console.log(storedData2);
+var names = ['Jack', 'Jill'];
+var stringArray = [];
+var anyArray = [];
+var p1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('done!');
+    }, 2000);
+});
+p1.then(function (data) { return data.split(' '); });
 //# sourceMappingURL=app.js.map
