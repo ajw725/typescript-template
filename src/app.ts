c66a75a18f825afed5814928f75baeb60e54e86a
Andrew Weinstein <andrew@altvia.com>
2020-10-10 17:34:18 -0600

    decorator factories

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,22 @@
 // decorators receive arguments!
 // when added to a class, there is one argument, which is the class's constructor
 // decorators run when the class is *defined*, not when it is instantiated!
-function Logger(constructor: Function) {
-  console.log('Logging...');
-  console.log(constructor);
+// function Logger(constructor: Function) {
+//   console.log('Logging...');
+//   console.log(constructor);
+// }
+
+// decorator factory allows us to customize behavior
+// it's basically a function that returns a decorator function, instead of just
+// the decorator function itself
+function Logger(logString: string) {
+  return function(constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  }
 }
 
-@Logger
+@Logger('LOGGING - PERSON')
 class Person {
   name = 'Andrew';
 
