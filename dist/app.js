3e97da155e62da141d6f1f5059942ea86aa1741d
Andrew Weinstein <andrew@altvia.com>
2020-10-10 10:52:12 -0600

    writing generic functions

--- dist/app.js
+++ dist/app.js
@@ -8,4 +8,13 @@ var p1 = new Promise(function (resolve, reject) {
     }, 2000);
 });
 p1.then(function (data) { return data.split(' '); });
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+var obj1 = { name: 'Andrew' };
+var obj2 = { age: 29 };
+var merged = merge(obj1, obj2);
+console.log(merged);
+console.log(merged.age);
+merge(obj1, obj2);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -13,3 +13,30 @@ const p1: Promise<string> = new Promise((resolve, reject) => {
 
 // specifying the promise type allows us to do stuff like this:
 p1.then(data => data.split(' '));
+
+// function merge(objA: object, objB: object) {
+//   return Object.assign(objA, objB);
+// }
+// const obj1 = { name: 'Andrew' };
+// const obj2 = { age: 29 };
+// console.log(merge(obj1, obj2)); 
+// // the above works, BUT if we try to store it...
+// const merged = merge(obj1, obj2);
+// merged.age doesn't work! because TS doesn't know the return type
+
+// turn merge into a generic function using two identifiers
+// "T" and "U" are just conventions - could use anything - to show that we are receiving
+// two objects potentially of DIFFERENT types!
+// TS interprets the return type as T & U
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const obj1 = { name: 'Andrew' };
+const obj2 = { age: 29 };
+const merged = merge(obj1, obj2);
+console.log(merged);
+console.log(merged.age);
+
+// you CAN fill in the types explicitly, like this, but it's redundant
+merge<{ name: string }, { age: number }>(obj1, obj2);
